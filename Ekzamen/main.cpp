73de48b66892c9aedc70a0495672345ea8a76e26
Gromakin Ivan <savana05555@gmail.com>
2022-05-22 18:54:48 +0300

    final version 1.1.0

--- Ekzamen/main.cpp
+++ Ekzamen/main.cpp
@@ -12,20 +12,32 @@ int main()
 {
 	srand(time(0));
 	setlocale(LC_ALL, "Russian");
-	char map[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+	HANDLE hin = GetStdHandle(STD_INPUT_HANDLE); // получаем дескриптор
+	INPUT_RECORD InputRecord; // используется для возвращения информации о входных сообщениях в консольном входном буфере
+	DWORD Events; // unsigned long
+	COORD coord; // для координат X, Y
+
+	/*Запретить выделение консоли*/
+	DWORD prev_mode;
+	GetConsoleMode(hin, &prev_mode);
+	SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), ENABLE_EXTENDED_FLAGS | (prev_mode & ~ENABLE_QUICK_EDIT_MODE));
+
+	char map[9] = { '#', '#', '#', '#', '#', '#', '#', '#', '#' };
 	HWND windowHandle = GetConsoleWindow();
 	ShowWindow(windowHandle, SW_MAXIMIZE);
-	int play1 = 0, play2 = 1;
-	/*
-	for (int i = 0; i < 256; i++)
+	int play1 = 0, play2 = 1,x = -1 , y = -1;
+	int mapmose[3][3];
+	int ch = 0;
+	for (int i = 0; i < 3; i++)
 	{
-		cout << i << " -> " << (char)i << "\t";
-		if (i%6==0)
+		for (int j = 0; j < 3; j++)
 		{
-			cout << "\n";
+			mapmose[i][j] = ch;
+			ch++;
 		}
 	}
-	*/
+
+
 	play1 = rand() % 2;
 	if (play1 == 1)
 	{
@@ -39,14 +51,11 @@ int main()
 	char sin = ' ';
 	for (int i = 0; true; i++)
 	{
-		int win = 0;
+		x = -1; y = -1;
+		SetConsoleMode(hin, ENABLE_MOUSE_INPUT); // разрешаем обработку мыши
 		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-		cout << "\t\t\t\t\t\t\t\t\t\t\t\t\t\tКлетоки: \n\t\t\t\t\t\t\t\t\t\t\t\t(цыфра на клавиатуре это клетке в игре)\n";
-		cout << "\t\t\t\t\t\t\t\t\t\t\t\t\t  1   |   2   |   3   " << endl;
-		cout << "\t\t\t\t\t\t\t\t\t\t\t\t\t  4   |   5   |   6   " << endl;
-		cout << "\t\t\t\t\t\t\t\t\t\t\t\t\t  7   |   8   |   9   " << endl;
 
-		cout << "\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t Игра\n" << endl;
+		cout << "\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t Игра\n\t\t\t\t\t\t\t\t\t\t\t\t\t      Click to [#]" << endl;
 		cout << "\t\t\t\t\t\t\t\t\t\t\t\t\t  " << map[0] << "   |   " << map[1] << "   |   " << map[2] << endl;
 		cout << "\t\t\t\t\t\t\t\t\t\t\t\t\t  " << map[3] << "   |   " << map[4] << "   |   " << map[5] << endl;
 		cout << "\t\t\t\t\t\t\t\t\t\t\t\t\t  " << map[6] << "   |   " << map[7] << "   |   " << map[8] << endl;
@@ -62,10 +71,11 @@ int main()
 			sin = 'O';
 		}
 		cout << "\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
+
 		// проверка горизонталей
 		for (int j = 0; j < 9; j += 3)
 		{
-			if (map[j] != ' ' && map[j] == map[j + 1] && map[j] == map[j + 2])
+			if (map[j] != '#' && map[j] == map[j + 1] && map[j] == map[j + 2])
 			{
 				goto Wins;
 			}
@@ -73,17 +83,17 @@ int main()
 		// прверка вертикалей
 		for (int j = 0; j < 3; j++)
 		{
-			if (map[j] != ' ' && map[j] == map[j + 3] && map[j] == map[j + 6])
+			if (map[j] != '#' && map[j] == map[j + 3] && map[j] == map[j + 6])
 			{
 				goto Wins;
 			}
 		}
 		// проверка крестом
-		if (map[0] != ' ' && map[0] == map[4] && map[0] == map[8])
+		if (map[0] != '#' && map[0] == map[4] && map[0] == map[8])
 		{
 			goto Wins;
 		}
-		if (map[2] != ' ' && map[2] == map[4] && map[2] == map[6])
+		if (map[2] != '#' && map[2] == map[4] && map[2] == map[6])
 		{
 			goto Wins;
 		}
@@ -103,7 +113,7 @@ int main()
 				system("cls");
 				for (int j = 0; j < 9; j++)
 				{
-					map[j] = ' ';
+					map[j] = '#';
 				}
 				i = 0;
 				ShowWindow(windowHandle, SW_MAXIMIZE);
@@ -121,7 +131,7 @@ int main()
 		close = 0;
 		for (int j = 0; j < 9; j++)
 		{
-			if (map[j]!=' ')
+			if (map[j] != '#')
 			{
 				close++;
 			}
@@ -145,50 +155,57 @@ int main()
 				i = 0;
 				ShowWindow(windowHandle, SW_MAXIMIZE);
 				continue;
+			}else {
+				system("cls");
+				cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t\t\tError.....\t\t\t\t\t";
+				Sleep(2000);
+				ShowWindow(windowHandle, SW_HIDE);
+				exit(0);
 			}
 
 		}
 
 
 		// Есле игра не оконченна
-		st = _getch();
-		while (st < 49 && st > 57)
-		{
-			cout << "Поле отсуствует";
-			st = _getch();
-			cout << "\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \bv\b \b\b \b\b \b\b \b\b \b\b \b";
-		}
-		switch (st)
-		{
-		case 49:
-			vod = 0;
-			break;
-		case 50:
-			vod = 1;
-			break;
-		case 51:
-			vod = 2;
-			break;
-		case 52:
-			vod = 3;
-			break;
-		case 53:
-			vod = 4;
-			break;
-		case 54:
-			vod = 5;
-			break;
-		case 55:
-			vod = 6;
-			break;
-		case 56:
-			vod = 7;
-			break;
-		case 57:
-			vod = 8;
-			break;
-		}
-		if (map[vod] != ' ')
+			while (true)
+			{
+				ReadConsoleInput(hin, &InputRecord, 1, &Events);
+				if (InputRecord.Event.MouseEvent.dwButtonState == MOUSE_WHEELED || InputRecord.Event.MouseEvent.dwButtonState == RIGHTMOST_BUTTON_PRESSED || InputRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+				{
+					coord.X = InputRecord.Event.MouseEvent.dwMousePosition.X;
+					coord.Y = InputRecord.Event.MouseEvent.dwMousePosition.Y;
+					break;
+				}
+			}
+			switch (coord.X)
+			{
+			case 106:
+				x = 0;
+				break;
+			case 114:
+				x = 1;
+				break;
+			case 122:
+				x = 2;
+				break;
+			default:
+				continue;
+			}
+			switch (coord.Y)
+			{
+			case 20:
+				y = 0;
+				break;
+			case 21:
+				y = 1;
+				break;
+			case 22:
+				y = 2;
+				break;
+			default:
+				continue;
+			}
+		if (map[mapmose[y][x]] != '#')
 		{
 			i--;
 			cout << "Поле не доступно";
@@ -196,10 +213,14 @@ int main()
 			system("cls");
 			continue;
 		}
-		map[vod] = sin;
+
+		map[mapmose[y][x]] = sin;
 		std::swap(play1, play2);
 		std::system("cls");
 	}
+
+	
+
 
 	return 0;
 }
